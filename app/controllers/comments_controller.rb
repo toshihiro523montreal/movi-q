898e68de07358c3cd3455f6d3a5420a19733e58c
toshihiro523montreal <toshihiro523montreal@gmail.comquitexitgit config --global user.name toshihiro523montreal>
2020-04-20 21:12:44 +0900

    stylecss9
    WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}"  # コメントと結びつくツイートの詳細画面に遷移する
+    # comment = Comment.create(comment_params)
+    # redirect_to "/posts/#{comment.post.id}"  # コメントと結びつくツイートの詳細画面に遷移する
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to post_path(params[:post_id])  }
+      format.json
+    end
   end
 
   private
